e46b82a6d2078b33052ff095ee831b8afb688d65
P9644 <111193708+Prathamesh596@users.noreply.github.com>
2025-10-02 16:49:30 +0530

    Delete src directory

--- src/api/categoryApi.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = "http://localhost:8080/api/categories"; // Your backend URL
-
-export const getAllCategories = () => axios.get(BASE_URL);
-export const getCategoryById = (id) => axios.get(`${BASE_URL}/${id}`);
-export const createCategory = (data) => axios.post(BASE_URL, data);
-export const updateCategory = (id, data) => axios.put(`${BASE_URL}/${id}`, data);
-export const deleteCategory = (id) => axios.delete(`${BASE_URL}/${id}`);
